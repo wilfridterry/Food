085b975dd12e805bedc2b43a228e869a0e048284
Stannis <zalevschi1998@gmail.com>
2022-06-20 23:48:26 +0300

    Added tabs

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item');
+    const tabsContent = document.querySelectorAll('.tabcontent');
+    const tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(el => {
+            el.classList.remove('show', 'fade');
+            el.classList.add('hide');
+        });
+
+        tabs.forEach(el => {
+            el.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.remove('hide');
+        tabsContent[i].classList.add('show', 'fade');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        if (e.target.classList.contains('tabheader__item')) {
+            tabs.forEach((el, i) => {
+                if (el == e.target) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
