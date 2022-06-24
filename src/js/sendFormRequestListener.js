be63762bc05105ba4b1a6fb2de5b9d9d402c97a3
Stannis <zalevschi1998@gmail.com>
2022-06-25 00:13:22 +0300

    Worked with sendFormRequest

--- src/js/sendFormRequestListener.js
+++ src/js/sendFormRequestListener.js
@@ -9,10 +9,23 @@ export function sendFormRequestListener(form) {
         spinnerElem.innerHTML = "SPINNER";
         form.insertAdjacentElement('afterend', spinnerElem);
 
-        setTimeout(() => {
-            form.reset();
-            spinnerElem.remove();
-            showThanksModal('Success. Thank you.');
-        }, 2000);
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => {
+                if (response.status > 400) {
+                    throw Error('Internal error');
+                }
+                return response.json();
+            })
+            .then(json => {
+                form.reset();
+                spinnerElem.remove();
+                showThanksModal('Success. Thank you.');
+            })
+            .catch((err) => {
+                showThanksModal(err);
+            })
+            .finally(() => {
+                form.reset();
+            });
     });
 }
