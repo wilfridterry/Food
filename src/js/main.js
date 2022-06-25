67d807736e37b5c6489e89a15de60ab949a8a34b
Stannis <zalevschi1998@gmail.com>
2022-06-26 01:45:01 +0300

    Added a basic slider

--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,46 @@ document.addEventListener("DOMContentLoaded", () => {
 
     bindFormRequestListeners();
     
+    initSlide();
 });
+
+function initSlide() {
+
+    const slides = document.querySelectorAll('.offer__slide');
+    let slideIndex = 0;
+
+    slides[slideIndex].style.display = 'block';
+    setCounterOfSlider();
+
+    document.querySelector('.offer__slider-next').addEventListener('click', () => {        
+        if (slideIndex == (slides.length-1)) {
+            return;
+        }
+
+        slides[slideIndex].style.display = 'none';
+        slides[slideIndex+1].style.display = 'block';
+        slideIndex++;
+        setCounterOfSlider();
+    });
+
+    document.querySelector('.offer__slider-prev').addEventListener('click', () => {
+        
+        if (slideIndex <= 0) {
+            return;
+        }
+
+        slides[slideIndex].style.display = 'none';
+        slides[slideIndex-1].style.display = 'block';
+        slideIndex--;
+        setCounterOfSlider();
+    });
+    
+    function setCounterOfSlider() {
+        document.querySelector('.offer__slider-counter>#current').innerHTML = (slideIndex+1);
+    }
+}
+
+
 
 
 
